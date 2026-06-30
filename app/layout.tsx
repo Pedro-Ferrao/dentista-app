324b1114ed030804df4e1d5cca62b7f548ab8625
Pedro Ferrão <ferraopedro10@gmail.com>
2026-06-30 16:25:30 -0300

    matadata title v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "dentista-app",
+  title: "Dentista App",
   description: "Portal de ferramentas da clínica Sorriso & Cia",
 };
 
